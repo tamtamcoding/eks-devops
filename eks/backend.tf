2e366393c3f218876f470066127d48ec53e05252
masterudu <rustamarifin5322@gmail.com>
2024-08-11 22:31:09 +0800

    modified

--- eks/backend.tf
+++ eks/backend.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "ezekiel-terra-state"
+    bucket         = "tamtamcoding-tfstate"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     //dynamodb_table = "MyTable"
